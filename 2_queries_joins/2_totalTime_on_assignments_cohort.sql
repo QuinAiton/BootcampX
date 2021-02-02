94f39c8318eee832c547a164463e425500d3c7a8
QuintenAiton <quinaiton@hotmail.com>
2021-02-02 00:26:08 +0000

    Total Time On Assignments by Cohort

---
+++ 2_queries_joins/2_totalTime_on_assignments_cohort.sql
@@ -0,0 +1,5 @@
+Select sum(assignment_submissions.duration) as assignment_duration
+FROM assignment_submissions 
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = 'FEB12';
