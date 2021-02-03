c924cdad9494184ca8dddad44dd89b05b4654916
QuintenAiton <quinaiton@hotmail.com>
2021-02-03 04:26:17 +0000

    Name Of Teachers that Assisted

--- 4_queries/12_name_teachers_assisted.sql
+++ 4_queries/12_name_teachers_assisted.sql
@@ -1,7 +1,7 @@
-Select distinct teachers.name as teachers_name, cohorts.name as cohorts_name
+Select distinct teachers.name as teacher, cohorts.name as cohort
 FROM assistance_requests
 JOIN teachers ON  teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
 WHERE cohorts.name = 'JUL02'
-ORDER BY teachers.name
+ORDER BY teachers.name 
